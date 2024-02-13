e1fcecdf82fa8de913d39e81e6930bfc1f87ecbb
Boffinbox <boffinbox@hotmail.co.uk>
2024-02-13 22:21:26 +0000

    add "my first block", i.e a "hello world" of minecraft modding

--- src/main/java/org/boff/works/RegistrationHandler.java
+++ src/main/java/org/boff/works/RegistrationHandler.java
@@ -1,9 +1,13 @@
 package org.boff.works;
 
+import org.boff.works.init.ModBlocks;
 import org.boff.works.util.RegistryUtil;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
 import net.minecraftforge.event.RegistryEvent.Register;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -19,6 +23,24 @@ public class RegistrationHandler
 				RegistryUtil.setItemName(new Item(), "first_item").setCreativeTab(CreativeTabs.MISC),
 				
 		};
+		
+		final Item[] itemBlocks = {
+				new ItemBlock(ModBlocks.FIRST_BLOCK).setRegistryName(ModBlocks.FIRST_BLOCK.getRegistryName())
+		};
+		
 		event.getRegistry().registerAll(items);
+		event.getRegistry().registerAll(itemBlocks);
+	}
+	
+	@SubscribeEvent
+	public static void registerBlocks(Register<Block> event)
+	{
+		final Block[] blocks =
+		{
+				RegistryUtil.setBlockName(new Block(Material.ROCK), "first_block").setCreativeTab(CreativeTabs.MISC),
+				
+		};
+		
+		event.getRegistry().registerAll(blocks);
 	}
 }
